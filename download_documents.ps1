2c4f8781f85b238c3f56c3a13502fef0cb600aa1
Drew Seewald <39170343+atseewal@users.noreply.github.com>
2024-04-16 23:22:19 -0400

    Clean up download script

--- download_documents.ps1
+++ download_documents.ps1
@@ -5,6 +5,4 @@ $dest = ".\data\"
 $url_2024_rules = "https://usapickleball.org/docs/USA-Pickleball-Official-Rulebook-2024-v1.pdf"
 $dest_2024_rules = ($dest + "USA_Pickleball_Rulebook_2024_v1.pdf")
 
-Write-Host $dest_2024_rules
-
 Invoke-WebRequest -Uri $url_2024_rules -OutFile $dest_2024_rules
