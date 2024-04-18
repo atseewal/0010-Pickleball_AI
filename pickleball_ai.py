1e1f5f793f867898ee64734116cd85cbe9a81657
Drew Seewald <39170343+atseewal@users.noreply.github.com>
2024-04-18 18:11:07 -0400

    Add PyPDFLoader

--- pickleball_ai.py
+++ pickleball_ai.py
@@ -2,3 +2,8 @@ import dotenv
 
 # Load environment variables
 dotenv.load_dotenv()
+
+from langchain_community.document_loaders import PyPDFLoader
+
+loader = PyPDFLoader("data/USA_Pickleball_Rulebook_2024_v1.pdf")
+pages = loader.load_and_split()
