9fcd78899ff770a7f8fab7ed0e41acbbfb4104e4
Drew Seewald <39170343+atseewal@users.noreply.github.com>
2024-04-18 21:43:20 -0400

    Create document embedding code

--- pickleball_ai.py
+++ pickleball_ai.py
@@ -1,9 +1,23 @@
 import dotenv
 
+from langchain_chroma import Chroma
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_openai import ChatOpenAI, OpenAIEmbeddings
+
 # Load environment variables
 dotenv.load_dotenv()
 
-from langchain_community.document_loaders import PyPDFLoader
+# Create LLM
+llm = ChatOpenAI(model="gpt-3.5-turbo")
 
 loader = PyPDFLoader("data/USA_Pickleball_Rulebook_2024_v1.pdf")
 pages = loader.load_and_split()
+
+create_new_vectors = False
+
+# Save vectors to disk
+if create_new_vectors:
+    vectorstore = Chroma.from_documents(documents=pages, embedding=OpenAIEmbeddings(), persist_directory="./chroma_db")
+
+# Load vectors to disk
+vectorstore = Chroma(persist_directory="./chroma_db", embedding_function=OpenAIEmbeddings())
