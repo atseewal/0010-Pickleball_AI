17b4bedb59bc3b4636993e743b03c2a4aa384d60
Drew Seewald <39170343+atseewal@users.noreply.github.com>
2024-04-18 22:07:29 -0400

    Finish generation code

--- pickleball_ai.py
+++ pickleball_ai.py
@@ -1,5 +1,8 @@
 import dotenv
 
+from langchain import hub
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.runnables import RunnablePassthrough
 from langchain_chroma import Chroma
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_openai import ChatOpenAI, OpenAIEmbeddings
@@ -13,11 +16,25 @@ llm = ChatOpenAI(model="gpt-3.5-turbo")
 loader = PyPDFLoader("data/USA_Pickleball_Rulebook_2024_v1.pdf")
 pages = loader.load_and_split()
 
+# To create new vectors and save to disk, set this to True
 create_new_vectors = False
 
-# Save vectors to disk
 if create_new_vectors:
     vectorstore = Chroma.from_documents(documents=pages, embedding=OpenAIEmbeddings(), persist_directory="./chroma_db")
 
 # Load vectors to disk
 vectorstore = Chroma(persist_directory="./chroma_db", embedding_function=OpenAIEmbeddings())
+
+# Retrieve and generate using the relevant snippets of the rulebook
+retriever = vectorstore.as_retriever(search_type="similarity", search_kwargs={"k": 3})
+prompt = hub.pull("rlm/rag-prompt")
+
+def format_docs(docs):
+    return "\n\n".join(doc.page_content for doc in docs)
+
+rag_chain = (
+    {"context": retriever | format_docs, "question": RunnablePassthrough()}
+    | prompt
+    | llm
+    | StrOutputParser()
+)
